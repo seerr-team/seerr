38532bd28af89c0cf55506aa7b49606626dc123f
Pierre <63404022+0-Pierre@users.noreply.github.com>
2025-03-14 19:00:00 +0100

    fix: lower maxRPS from 25 to 1 to prevent hitting rate limits

--- server/api/musicbrainz/index.ts
+++ server/api/musicbrainz/index.ts
@@ -20,7 +20,7 @@ class MusicBrainz extends ExternalAPI {
         },
         nodeCache: cacheManager.getCache('musicbrainz').data,
         rateLimit: {
-          maxRPS: 25,
+          maxRPS: 1,
           id: 'musicbrainz',
         },
       }
