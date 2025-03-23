3e08771c6a8d1d33f95e75cf1ff07c6dbb12407b
Pierre <63404022+0-Pierre@users.noreply.github.com>
2025-03-23 14:07:22 +0100

    refactor: move lidarrServiceId column creation to a separate migration file

---
+++ server/migration/sqlite/1742734853146-AddLidarrServiceIdToOverrideRules.ts
@@ -0,0 +1,19 @@
+import type { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddLidarrServiceIdToOverrideRules1742734853146
+  implements MigrationInterface
+{
+  name = 'AddLidarrServiceIdToOverrideRules1742734853146';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "override_rule" ADD "lidarrServiceId" integer`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "override_rule" DROP COLUMN "lidarrServiceId"`
+    );
+  }
+}
