f40a8eb03d3e89d35bc318c8733cd2ca52ec796e
0xsysr3ll <0xsysr3ll@pm.me>
2025-04-21 19:31:59 +0200

    fix(db): re-generate correct database migrations

---
+++ server/migration/postgres/1745254842580-AddPlexProfilesSupport.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddPlexProfilesSupport1745254842580 implements MigrationInterface {
+    name = 'AddPlexProfilesSupport1745254842580'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" ADD "plexProfileId" character varying`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "isPlexProfile" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "mainPlexUserId" integer`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "avatarETag" character varying`);
+        await queryRunner.query(`ALTER TABLE "user" ADD "avatarVersion" character varying`);
+        await queryRunner.query(`ALTER TABLE "blacklist" ADD "blacklistedTags" character varying`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "blacklist" DROP COLUMN "blacklistedTags"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "avatarVersion"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "avatarETag"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "mainPlexUserId"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isPlexProfile"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "plexProfileId"`);
+    }
+
+}
