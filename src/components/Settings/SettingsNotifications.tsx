0d6fc395502fb90b134cf35b29ba1b2b7063b9a5
Schrottfresser <39998368+Schrottfresser@users.noreply.github.com>
2025-04-25 00:30:11 +0200

    feat(notifications): implement basic settings notifications page

--- src/components/Settings/SettingsNotifications.tsx
+++ src/components/Settings/SettingsNotifications.tsx
@@ -1,38 +1,139 @@
+import Button from '@app/components/Common/Button';
+import Header from '@app/components/Common/Header';
+import LoadingSpinner from '@app/components/Common/LoadingSpinner';
 import PageTitle from '@app/components/Common/PageTitle';
 import Table from '@app/components/Common/Table';
+import { useUpdateQueryParams } from '@app/hooks/useUpdateQueryParams';
 import globalMessages from '@app/i18n/globalMessages';
 import defineMessages from '@app/utils/defineMessages';
+import {
+  BarsArrowDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from '@heroicons/react/24/solid';
+import type { NotificationSettingsResultResponse } from '@server/interfaces/api/settingsInterfaces';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { useIntl } from 'react-intl';
+import { useToasts } from 'react-toast-notifications';
+import useSWR from 'swr';
 
 const messages = defineMessages('components.Settings', {
   notifications: 'Notifications',
-  notificationSettings: 'Notification Settings',
-  notificationSettingsDescription:
-    'Configure and enable global notification agents.',
+  notificationInstanceList: 'Notification Instance List',
   instanceName: 'Name',
   instanceId: 'ID',
   notificationAgent: 'Agent',
+  instanceDeleted: 'Notification instance deleted successfully!',
+  instanceDeleteError:
+    'Something went wrong while deleting the notification instance.',
   email: 'Email',
   webhook: 'Webhook',
   webpush: 'Web Push',
 });
 
+enum Sort {
+  ID = 'id',
+  NAME = 'name',
+  AGENT = 'agent',
+}
+
 const SettingsNotifications = () => {
   const intl = useIntl();
+  const router = useRouter();
+  const { addToast } = useToasts();
+  const [currentSort, setCurrentSort] = useState<Sort>(Sort.ID);
+  const [currentPageSize, setCurrentPageSize] = useState<number>(10);
+  const [, setDeleting] = useState(false);
+  const [selectedInstances, setSelectedInstances] = useState<number[]>([]);
 
-  /*const {
-    data,
-    error,
-    mutate: revalidate,
-  } = useSWR(
-    `/api/v1/settings/notifications?take=${currentPageSize}&skip=${
-      pageIndex * currentPageSize
-    }&sort=${currentSort}`
-  );
+  const page = router.query.page ? Number(router.query.page) : 1;
+  const pageIndex = page - 1;
+  const updateQueryParams = useUpdateQueryParams({ page: page.toString() });
 
-  if (!data && !error) {
+  const { data, mutate: revalidate } =
+    useSWR<NotificationSettingsResultResponse>(
+      `/api/v1/settings/notification?take=${currentPageSize}&skip=${
+        pageIndex * currentPageSize
+      }&sort=${currentSort}`
+    );
+
+  useEffect(() => {
+    const filterString = window.localStorage.getItem('nl-filter-settings');
+
+    if (filterString) {
+      const filterSettings = JSON.parse(filterString);
+
+      setCurrentSort(filterSettings.currentSort);
+      setCurrentPageSize(filterSettings.currentPageSize);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem(
+      'nl-filter-settings',
+      JSON.stringify({
+        currentSort,
+        currentPageSize,
+      })
+    );
+  }, [currentSort, currentPageSize]);
+
+  const isAllInstancesSelected = () =>
+    selectedInstances.length === data?.results.length;
+  const isInstanceSelected = (instanceId: number) =>
+    selectedInstances.includes(instanceId);
+
+  const toggleAllInstances = () => {
+    if (
+      data &&
+      selectedInstances.length >= 0 &&
+      selectedInstances.length < data.results.length - 1
+    ) {
+      setSelectedInstances(data.results.map((instance) => instance.id));
+    } else {
+      setSelectedInstances([]);
+    }
+  };
+
+  const toggleInstance = (instanceId: number) => {
+    if (selectedInstances.includes(instanceId)) {
+      setSelectedInstances((instances) =>
+        instances.filter((u) => u !== instanceId)
+      );
+    } else {
+      setSelectedInstances((instances) => [...instances, instanceId]);
+    }
+  };
+
+  const deleteInstance = async (instanceId: number) => {
+    setDeleting(true);
+
+    try {
+      await axios.delete(`/api/v1/settings/notification/${instanceId}`);
+
+      addToast(intl.formatMessage(messages.instanceDeleted), {
+        autoDismiss: true,
+        appearance: 'success',
+      });
+    } catch (e) {
+      addToast(intl.formatMessage(messages.instanceDeleteError), {
+        autoDismiss: true,
+        appearance: 'error',
+      });
+    } finally {
+      setDeleting(false);
+      revalidate();
+    }
+  };
+
+  if (!data) {
     return <LoadingSpinner />;
-  }*/
+  }
+
+  const hasNextPage = data.pageInfo.pages > pageIndex + 1;
+  const hasPrevPage = pageIndex > 0;
 
   return (
     <>
@@ -43,38 +144,185 @@ const SettingsNotifications = () => {
         ]}
       />
 
-      <div className="mb-6">
-        <h3 className="heading">
-          {intl.formatMessage(messages.notificationSettings)}
-        </h3>
-        <p className="description">
-          {intl.formatMessage(messages.notificationSettingsDescription)}
-        </p>
+      <div className="flex flex-col justify-between lg:flex-row lg:items-end">
+        <Header>{intl.formatMessage(messages.notificationInstanceList)}</Header>
+        <div className="mt-2 flex flex-grow flex-col lg:flex-grow-0 lg:flex-row">
+          <div className="mb-2 flex flex-grow flex-col justify-between sm:flex-row lg:mb-0 lg:flex-grow-0">
+            {/*<Button
+                className="mb-2 flex-grow sm:mb-0 sm:mr-2"
+                buttonType="primary"
+                onClick={() => setCreateModal({ isOpen: true })}
+              >
+                <UserPlusIcon />
+                <span>{intl.formatMessage(messages.createlocaluser)}</span>
+              </Button>*/}
+          </div>
+          <div className="mb-2 flex flex-grow lg:mb-0 lg:flex-grow-0">
+            <span className="inline-flex cursor-default items-center rounded-l-md border border-r-0 border-gray-500 bg-gray-800 px-3 text-sm text-gray-100">
+              <BarsArrowDownIcon className="h-6 w-6" />
+            </span>
+            <select
+              id="sort"
+              name="sort"
+              onChange={(e) => {
+                setCurrentSort(e.target.value as Sort);
+                router.push(router.pathname);
+              }}
+              value={currentSort}
+              className="rounded-r-only"
+            >
+              <option value="id">
+                {intl.formatMessage(messages.instanceId)}
+              </option>
+              <option value="name">
+                {intl.formatMessage(messages.instanceName)}
+              </option>
+              <option value="agent">
+                {intl.formatMessage(messages.notificationAgent)}
+              </option>
+            </select>
+          </div>
+        </div>
       </div>
 
       <Table>
         <thead>
           <tr>
             <Table.TH>
-              <input type="checkbox" id="selectAll" name="selectAll" />
+              {(data.results ?? []).length > 1 && (
+                <input
+                  type="checkbox"
+                  id="selectAll"
+                  name="selectAll"
+                  checked={isAllInstancesSelected()}
+                  onChange={() => {
+                    toggleAllInstances();
+                  }}
+                />
+              )}
             </Table.TH>
             <Table.TH>{intl.formatMessage(messages.instanceName)}</Table.TH>
             <Table.TH>{intl.formatMessage(messages.instanceId)}</Table.TH>
             <Table.TH>
               {intl.formatMessage(messages.notificationAgent)}
             </Table.TH>
+            <Table.TH></Table.TH>
           </tr>
         </thead>
 
         <Table.TBody>
-          {
-            <tr>
-              <Table.TD></Table.TD>
-              <Table.TD></Table.TD>
-              <Table.TD></Table.TD>
-              <Table.TD></Table.TD>
+          {data.results.map((instance) => (
+            <tr
+              key={`notification-instance-list-${instance.id}`}
+              data-testid="notification-instance-list-row"
+            >
+              <Table.TD>
+                <input
+                  type="checkbox"
+                  id={`notification-instance-list-select-${instance.id}`}
+                  name={`notification-instance-list-select-${instance.id}`}
+                  checked={isInstanceSelected(instance.id)}
+                  onChange={() => {
+                    toggleInstance(instance.id);
+                  }}
+                />
+              </Table.TD>
+              <Table.TD>{instance.name}</Table.TD>
+              <Table.TD>{instance.id}</Table.TD>
+              <Table.TD>{instance.agent}</Table.TD>
+              <Table.TD alignText="right">
+                <Button
+                  buttonType="warning"
+                  className="mr-2"
+                  onClick={() =>
+                    router.push(
+                      '/users/[userId]/settings',
+                      `/users/${instance.id}/settings`
+                    )
+                  }
+                >
+                  {intl.formatMessage(globalMessages.edit)}
+                </Button>
+                <Button
+                  buttonType="danger"
+                  onClick={() => deleteInstance(instance.id)}
+                >
+                  {intl.formatMessage(globalMessages.delete)}
+                </Button>
+              </Table.TD>
             </tr>
-          }
+          ))}
+          <tr className="bg-gray-700">
+            <Table.TD colSpan={8} noPadding>
+              <nav
+                className="flex w-screen flex-col items-center space-x-4 space-y-3 px-6 py-3 sm:flex-row sm:space-y-0 lg:w-full"
+                aria-label="Pagination"
+              >
+                <div className="hidden lg:flex lg:flex-1">
+                  <p className="text-sm">
+                    {data.results.length > 0 &&
+                      intl.formatMessage(globalMessages.showingresults, {
+                        from: pageIndex * currentPageSize + 1,
+                        to:
+                          data.results.length < currentPageSize
+                            ? pageIndex * currentPageSize + data.results.length
+                            : (pageIndex + 1) * currentPageSize,
+                        total: data.pageInfo.results,
+                        strong: (msg: React.ReactNode) => (
+                          <span className="font-medium">{msg}</span>
+                        ),
+                      })}
+                  </p>
+                </div>
+                <div className="flex justify-center sm:flex-1 sm:justify-start lg:justify-center">
+                  <span className="-mt-3 items-center text-sm sm:-ml-4 sm:mt-0 lg:ml-0">
+                    {intl.formatMessage(globalMessages.resultsperpage, {
+                      pageSize: (
+                        <select
+                          id="pageSize"
+                          name="pageSize"
+                          onChange={(e) => {
+                            setCurrentPageSize(Number(e.target.value));
+                            router
+                              .push(router.pathname)
+                              .then(() => window.scrollTo(0, 0));
+                          }}
+                          value={currentPageSize}
+                          className="short inline"
+                        >
+                          <option value="5">5</option>
+                          <option value="10">10</option>
+                          <option value="25">25</option>
+                          <option value="50">50</option>
+                          <option value="100">100</option>
+                        </select>
+                      ),
+                    })}
+                  </span>
+                </div>
+                <div className="flex flex-auto justify-center space-x-2 sm:flex-1 sm:justify-end">
+                  <Button
+                    disabled={!hasPrevPage}
+                    onClick={() =>
+                      updateQueryParams('page', (page - 1).toString())
+                    }
+                  >
+                    <ChevronLeftIcon />
+                    <span>{intl.formatMessage(globalMessages.previous)}</span>
+                  </Button>
+                  <Button
+                    disabled={!hasNextPage}
+                    onClick={() =>
+                      updateQueryParams('page', (page + 1).toString())
+                    }
+                  >
+                    <span>{intl.formatMessage(globalMessages.next)}</span>
+                    <ChevronRightIcon />
+                  </Button>
+                </div>
+              </nav>
+            </Table.TD>
+          </tr>
         </Table.TBody>
       </Table>
     </>
