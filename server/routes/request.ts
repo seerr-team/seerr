7f3bcad4ec159104199aeb87edec2f811111a9d2
Fabian <ornelasf1@gmail.com>
2025-05-05 22:25:08 -0500

    style: format

--- server/routes/request.ts
+++ server/routes/request.ts
@@ -689,7 +689,7 @@ requestRoutes.post<{
         relations: { requestedBy: true, modifiedBy: true },
       });
 
-      const declineReason: string = req.body ? req.body.declineReason : "";
+      const declineReason: string = req.body ? req.body.declineReason : '';
       let newStatus: MediaRequestStatus;
 
       switch (req.params.status) {
