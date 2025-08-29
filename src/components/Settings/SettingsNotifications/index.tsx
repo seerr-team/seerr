2af3d3747a010f26acb22b4ab95eb80b8ed67d69
Schrottfresser <39998368+Schrottfresser@users.noreply.github.com>
2025-08-29 20:16:58 +0200

    feat(notifications): extract NotificationInstanceDropdown component

--- src/components/Settings/SettingsNotifications/index.tsx
+++ src/components/Settings/SettingsNotifications/index.tsx
@@ -1,14 +1,7 @@
-import DiscordLogo from '@app/assets/extlogos/discord.svg';
-import GotifyLogo from '@app/assets/extlogos/gotify.svg';
-import NtfyLogo from '@app/assets/extlogos/ntfy.svg';
-import PushbulletLogo from '@app/assets/extlogos/pushbullet.svg';
-import PushoverLogo from '@app/assets/extlogos/pushover.svg';
-import SlackLogo from '@app/assets/extlogos/slack.svg';
-import TelegramLogo from '@app/assets/extlogos/telegram.svg';
-import Dropdown from '@app/components/Common/Dropdown';
 import Header from '@app/components/Common/Header';
 import LoadingSpinner from '@app/components/Common/LoadingSpinner';
 import PageTitle from '@app/components/Common/PageTitle';
+import NotificationInstanceDropdown from '@app/components/Settings/SettingsNotifications/NotificationInstanceDropdown';
 import NotificationInstanceList from '@app/components/Settings/SettingsNotifications/NotificationInstanceList';
 import NotificationModal, {
   NotificationModalType,
@@ -17,15 +10,13 @@ import { useUpdateQueryParams } from '@app/hooks/useUpdateQueryParams';
 import globalMessages from '@app/i18n/globalMessages';
 import defineMessages from '@app/utils/defineMessages';
 import { Transition } from '@headlessui/react';
-import {
-  BarsArrowDownIcon,
-  BoltIcon,
-  CloudIcon,
-  EnvelopeIcon,
-  PlusIcon,
-} from '@heroicons/react/24/solid';
+import { BarsArrowDownIcon } from '@heroicons/react/24/solid';
 import type { NotificationSettingsResultResponse } from '@server/interfaces/api/settingsInterfaces';
-import type { NotificationAgentConfig } from '@server/interfaces/settings';
+import type {
+  NotificationAgentConfig,
+  NotificationAgentKey,
+  NotificationAgentTemplates,
+} from '@server/interfaces/settings';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { useIntl } from 'react-intl';
@@ -34,7 +25,6 @@ import useSWR from 'swr';
 const messages = defineMessages('components.Settings.SettingsNotifications', {
   notifications: 'Notifications',
   notificationInstanceList: 'Notification Instance List',
-  createNotificationInstance: 'Create Notification Instance',
   instanceName: 'Name',
   instanceId: 'ID',
   notificationAgent: 'Agent',
@@ -100,6 +90,15 @@ const SettingsNotifications = () => {
     });
   };
 
+  const onNotificationInstanceCreate = (agentKey: NotificationAgentKey) => {
+    setNotificationModal({
+      open: true,
+      instance:
+        data?.agentTemplates[agentKey as keyof NotificationAgentTemplates],
+      type: NotificationModalType.CREATE,
+    });
+  };
+
   if (!data) {
     return <LoadingSpinner />;
   }
@@ -150,139 +149,10 @@ const SettingsNotifications = () => {
       <div className="flex flex-col justify-between lg:flex-row lg:items-end">
         <Header>{intl.formatMessage(messages.notificationInstanceList)}</Header>
         <div className="mt-2 flex flex-grow flex-col lg:flex-grow-0 lg:flex-row">
-          <div className="mb-2 flex-grow justify-between sm:mr-2 sm:flex-row lg:mb-0 lg:flex-grow-0">
-            <Dropdown
-              className="mb-2 flex-grow sm:mb-0"
-              buttonType="ghost"
-              text={
-                <>
-                  <PlusIcon />
-                  {intl.formatMessage(messages.createNotificationInstance)}
-                </>
-              }
-            >
-              <Dropdown.Item
-                onClick={() =>
-                  setNotificationModal({
-                    open: true,
-                    instance: data.agentTemplates.discord,
-                    type: NotificationModalType.CREATE,
-                  })
-                }
-              >
-                <DiscordLogo />
-                <span>Discord</span>
-              </Dropdown.Item>
-              <Dropdown.Item
-                onClick={() =>
-                  setNotificationModal({
-                    open: true,
-                    instance: data.agentTemplates.email,
-                    type: NotificationModalType.CREATE,
-                  })
-                }
-              >
-                <EnvelopeIcon />
-                <span>Email</span>
-              </Dropdown.Item>
-              <Dropdown.Item
-                onClick={() =>
-                  setNotificationModal({
-                    open: true,
-                    instance: data.agentTemplates.gotify,
-                    type: NotificationModalType.CREATE,
-                  })
-                }
-              >
-                <GotifyLogo />
-                <span>Gotify</span>
-              </Dropdown.Item>
-              <Dropdown.Item
-                onClick={() =>
-                  setNotificationModal({
-                    open: true,
-                    instance: data.agentTemplates.ntfy,
-                    type: NotificationModalType.CREATE,
-                  })
-                }
-              >
-                <NtfyLogo />
-                <span>Ntfy</span>
-              </Dropdown.Item>
-              <Dropdown.Item
-                onClick={() =>
-                  setNotificationModal({
-                    open: true,
-                    instance: data.agentTemplates.pushbullet,
-                    type: NotificationModalType.CREATE,
-                  })
-                }
-              >
-                <PushbulletLogo />
-                <span>Pushbullet</span>
-              </Dropdown.Item>
-              <Dropdown.Item
-                onClick={() =>
-                  setNotificationModal({
-                    open: true,
-                    instance: data.agentTemplates.pushover,
-                    type: NotificationModalType.CREATE,
-                  })
-                }
-              >
-                <PushoverLogo />
-                <span>Pushover</span>
-              </Dropdown.Item>
-              <Dropdown.Item
-                onClick={() =>
-                  setNotificationModal({
-                    open: true,
-                    instance: data.agentTemplates.slack,
-                    type: NotificationModalType.CREATE,
-                  })
-                }
-              >
-                <SlackLogo />
-                <span>Slack</span>
-              </Dropdown.Item>
-              <Dropdown.Item
-                onClick={() =>
-                  setNotificationModal({
-                    open: true,
-                    instance: data.agentTemplates.telegram,
-                    type: NotificationModalType.CREATE,
-                  })
-                }
-              >
-                <TelegramLogo />
-                <span>Telegram</span>
-              </Dropdown.Item>
-              <Dropdown.Item
-                onClick={() =>
-                  setNotificationModal({
-                    open: true,
-                    instance: data.agentTemplates.webhook,
-                    type: NotificationModalType.CREATE,
-                  })
-                }
-              >
-                <BoltIcon />
-                <span>Webhook</span>
-              </Dropdown.Item>
-              <Dropdown.Item
-                onClick={() =>
-                  setNotificationModal({
-                    open: true,
-                    instance: data.agentTemplates.webpush,
-                    type: NotificationModalType.CREATE,
-                  })
-                }
-              >
-                <CloudIcon />
-                <span>WebPush</span>
-              </Dropdown.Item>
-            </Dropdown>
-          </div>
+          <NotificationInstanceDropdown
+            onSelect={onNotificationInstanceCreate}
+          />
+
           <div className="mb-2 flex flex-grow lg:mb-0 lg:flex-grow-0">
             <span className="inline-flex cursor-default items-center rounded-l-md border border-r-0 border-gray-500 bg-gray-800 px-3 text-sm text-gray-100">
               <BarsArrowDownIcon className="h-6 w-6" />
