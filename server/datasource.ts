59368fa065c0f379dfb4e37c67fa38ca8c5c8f13
gauthier-th <mail@gauthierth.fr>
2025-10-22 14:33:59 +0200

    fix: remove unwanterd change on postgres dev datasource

--- server/datasource.ts
+++ server/datasource.ts
@@ -77,7 +77,7 @@ const postgresDevConfig: DataSourceOptions = {
   database: process.env.DB_NAME ?? 'seerr',
   ssl: buildSslConfig(),
   synchronize: false,
-  migrationsRun: false,
+  migrationsRun: true,
   logging: boolFromEnv('DB_LOG_QUERIES'),
   entities: ['server/entity/**/*.ts'],
   migrations: ['server/migration/postgres/**/*.ts'],
