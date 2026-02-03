5ffabb951a8f05fbf7676aa83b4561c71a5e4c90
gauthier-th <mail@gauthierth.fr>
2026-02-03 14:23:27 +0100

    style: run prettier

--- server/migration/postgres/1746900000000-RenameBlacklistToBlocklist.ts
+++ server/migration/postgres/1746900000000-RenameBlacklistToBlocklist.ts
@@ -1,8 +1,6 @@
 import type { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class RenameBlacklistToBlocklist1746900000000
-  implements MigrationInterface
-{
+export class RenameBlacklistToBlocklist1746900000000 implements MigrationInterface {
   name = 'RenameBlacklistToBlocklist1746900000000';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
