5fcaa700cf0da7bd60cd1c11c3aa9c52a72eacfb
Michael Thomas <michaelhthomas@outlook.com>
2026-02-09 00:18:37 -0500

    feat: support login with OpenID Connect

--- server/index.ts
+++ server/index.ts
@@ -250,7 +250,12 @@ app
     server.get('*path', (req, res) => handle(req, res));
     server.use(
       (
-        err: { status: number; message: string; errors: string[] },
+        err: {
+          status: number;
+          message: string;
+          errors: string[];
+          error?: string;
+        },
         _req: Request,
         res: Response,
         // We must provide a next function for the function signature here even though its not used
@@ -261,6 +266,7 @@ app
         res.status(err.status || 500).json({
           message: err.message,
           errors: err.errors,
+          ...(err.error != null && { error: err.error }),
         });
       }
     );
