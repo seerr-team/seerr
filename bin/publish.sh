1db4abff8c55793f9e31d294771d163e88c39a2b
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2026-02-15 00:42:18 +0100

    chore: add a script to automated release process
    Signed-off-by: Ludovic Ortega <ludovic.ortega@adminafk.fr>

---
+++ bin/publish.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Check arguments
+if [ "$#" -ne 1 ]; then
+  echo -e "Usage: $0 <version>\n\nExample: $0 1.2.3"
+  exit 1
+fi
+
+# Update package.json, commit the file and create a tag
+npm version $1 -m "chore: update version to v$1" --sign-git-tag
