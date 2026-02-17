9e27c36ab2256e467afac731c58983abecd19102
Michael Thomas <michaelhthomas@outlook.com>
2026-02-17 08:36:56 -0500

    fix(auth): await calls to userRepository.save

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -739,7 +739,7 @@ authRoutes.post('/reset-password', async (req, res, next) => {
 
   if (user) {
     await user.resetPassword();
-    userRepository.save(user);
+    await userRepository.save(user);
     logger.info('Successfully sent password reset link', {
       label: 'API',
       ip: req.ip,
@@ -804,7 +804,7 @@ authRoutes.post('/reset-password/:guid', async (req, res, next) => {
   }
   user.recoveryLinkExpirationDate = null;
   await user.setPassword(req.body.password);
-  userRepository.save(user);
+  await userRepository.save(user);
   logger.info('Successfully reset password', {
     label: 'API',
     ip: req.ip,
