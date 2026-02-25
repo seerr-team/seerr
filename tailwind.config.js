3b6f8aba9b2d507e9cee8a57a1c0d6bb26648af5
Pierre Spring <pierre.spring@caillou.ch>
2026-02-25 15:22:36 +0100

    chore(tailwind): font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  content: [
-    './node_modules/@seerr-team/react-tailwindcss-datepicker/dist/index.esm.js',
-    './src/pages/**/*.{ts,tsx}',
-    './src/components/**/*.{ts,tsx}',
-  ],
   theme: {
     extend: {
       transitionProperty: {
         'max-height': 'max-height',
         width: 'width',
-      },
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
     },
   },
