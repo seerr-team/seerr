10ea21b20ff696768424b64f6fd834c8e3e24108
Pierre Spring <pierre.spring@caillou.ch>
2026-02-27 10:48:48 +0100

    feat: self-host font for better privacy (#2540)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,7 @@ import type { User } from '@app/hooks/useUser';
 import { Permission, useUser } from '@app/hooks/useUser';
 import '@app/styles/globals.css';
 import { polyfillIntl } from '@app/utils/polyfillIntl';
+import '@fontsource-variable/inter';
 import { MediaServerType } from '@server/constants/server';
 import type { PublicSettingsResponse } from '@server/interfaces/api/settingsInterfaces';
 import type { AvailableLocale } from '@server/types/languages';
