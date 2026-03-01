7759065dcca104e2e76775fc802863a7a2ebf668
Kyle Ouellette <kyleouellette22@gmail.com>
2026-03-01 15:37:25 -0500

    fix: support for IPv6 address literals in HOST env var
    When pages would build URLs for API requests, process.env.HOST would be directly concatenated with
    process.env.PORT (e.g., `0.0.0.0:5055`). This works for IPv4 address literals but does not work
    for IPv6 address literals because `:::5055` for example is invalid. This commit adds a check if
    process.env.HOST contains a colon (:) and if so, wraps the host in square brackets. E.g.,
    `[::]:5055`, which is valid. This allows for users to set the HOST environment variable to IPv6
    address literals.
    
    fix #2612

---
+++ src/utils/urlHelper.ts
@@ -0,0 +1,10 @@
+export function getHostAndPort(): string {
+  let host = process.env.HOST || 'localhost';
+  if (host.includes(':')) {
+    // If host includes a colon, it's an IPv6 literal and needs to be placed in square brackets
+    host = `[${host}]`;
+  }
+
+  const port = process.env.PORT || 5055;
+  return `${host}:${port}`;
+}
