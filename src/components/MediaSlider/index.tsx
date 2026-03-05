469f98c87e2e12a940c450b8d148767f8c31ae99
0xsysr3ll <0xsysr3ll@pm.me>
2026-03-05 21:55:46 +0100

    fix: don't hide results other than tv/movie

--- src/components/MediaSlider/index.tsx
+++ src/components/MediaSlider/index.tsx
@@ -83,14 +83,18 @@ const MediaSlider = ({
   }
 
   if (settings.currentSettings.hideRequested) {
-    titles = titles.filter(
-      (i) =>
-        (i.mediaType === 'movie' || i.mediaType === 'tv') &&
-        (i.mediaInfo?.status === MediaStatus.AVAILABLE ||
-          i.mediaInfo?.status === MediaStatus.PARTIALLY_AVAILABLE ||
-          !i.mediaInfo?.requests ||
-          i.mediaInfo.requests.length === 0)
-    );
+    titles = titles.filter((i) => {
+      if (i.mediaType !== 'movie' && i.mediaType !== 'tv') {
+        return true;
+      }
+
+      return (
+        i.mediaInfo?.status === MediaStatus.AVAILABLE ||
+        i.mediaInfo?.status === MediaStatus.PARTIALLY_AVAILABLE ||
+        !i.mediaInfo?.requests ||
+        i.mediaInfo.requests.length === 0
+      );
+    });
   }
 
   useEffect(() => {
