00f5e2de62f9e89ca792d02a39265b546d3bf602
Benja Share <benja1006@gmail.com>
2026-03-06 11:15:01 -0800

    Added descriptive try/catch per CodeRabbit's recommendation

--- server/utils/env.ts
+++ server/utils/env.ts
@@ -6,7 +6,12 @@ export function stringOrReadFileFromEnv(envVar: string): string | undefined {
     }
     const filePath = process.env[`${envVar}_FILE`];
     if (filePath) {
-        return fs.readFileSync(filePath, 'utf-8');
+        try {
+            return fs.readFileSync(filePath, 'utf-8');
+        } catch (err) {
+            throw new Error(`ENV ${envVar}_FILE was defined but was unable to be read from ${filePath}: ${(err as Error).message}`)
+        }
+
     }
     return undefined;
 }
