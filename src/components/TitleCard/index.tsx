175338a845ed9135f3c8c8a51805450141098b42
0xsysr3ll <0xsysr3ll@pm.me>
2026-03-16 23:25:21 +0100

    fix(blocklist): wrong api for collection blocklist

--- src/components/TitleCard/index.tsx
+++ src/components/TitleCard/index.tsx
@@ -177,7 +177,7 @@ const TitleCard = ({
     if (topNode) {
       try {
         if (mediaType === 'collection') {
-          await axios.post(`/api/v1/blacklist/collection/${id}`);
+          await axios.post(`/api/v1/blocklist/collection/${id}`);
         } else {
           await axios.post('/api/v1/blocklist', {
             tmdbId: id,
