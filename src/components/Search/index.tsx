3b16a3aeea6a4e65aaeb23ee035bd55f7687cc8e
Robin Ting <civ2boss@gmail.com>
2026-03-18 09:35:57 -0400

    feat(component): add filters to search results page
    - add filters to search results for all, movies, tvshows, people, or collections

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,23 +2,32 @@ import Header from '@app/components/Common/Header';
 import ListView from '@app/components/Common/ListView';
 import PageTitle from '@app/components/Common/PageTitle';
 import useDiscover from '@app/hooks/useDiscover';
+import globalMessages from '@app/i18n/globalMessages';
 import ErrorPage from '@app/pages/_error';
 import defineMessages from '@app/utils/defineMessages';
+import { CircleStackIcon } from '@heroicons/react/24/solid';
 import type {
+  CollectionResult,
   MovieResult,
   PersonResult,
   TvResult,
 } from '@server/models/Search';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { useIntl } from 'react-intl';
 
 const messages = defineMessages('components.Search', {
   search: 'Search',
   searchresults: 'Search Results',
+  timeWindowDay: 'Daily',
+  timeWindowWeek: 'Weekly',
 });
+
+type SearchType = 'all' | 'movie' | 'tv' | 'person' | 'collection';
 
 const Search = () => {
   const intl = useIntl();
+  const [currentSearchType, setCurrentSearchType] = useState<SearchType>('all');
   const router = useRouter();
 
   const {
@@ -29,10 +38,11 @@ const Search = () => {
     titles,
     fetchMore,
     error,
-  } = useDiscover<MovieResult | TvResult | PersonResult>(
+  } = useDiscover<MovieResult | TvResult | PersonResult | CollectionResult>(
     `/api/v1/search`,
     {
       query: router.query.query,
+      searchType: currentSearchType,
     },
     { hideAvailable: false, hideBlocklisted: false }
   );
@@ -44,8 +54,40 @@ const Search = () => {
   return (
     <>
       <PageTitle title={intl.formatMessage(messages.search)} />
-      <div className="mb-5 mt-1">
+      <div className="mb-5 mt-1 flex flex-col justify-between lg:flex-row lg:items-end">
         <Header>{intl.formatMessage(messages.searchresults)}</Header>
+        <div className="mt-2 flex flex-grow flex-col sm:flex-row lg:flex-grow-0">
+          <div className="mb-2 flex flex-grow sm:mb-0 sm:mr-2 lg:flex-grow-0">
+            <span className="inline-flex cursor-default items-center rounded-l-md border border-r-0 border-gray-500 bg-gray-800 px-3 text-sm text-gray-100">
+              <CircleStackIcon className="h-6 w-6" />
+            </span>
+            <select
+              id="searchType"
+              name="searchType"
+              onChange={(e) =>
+                setCurrentSearchType(e.target.value as SearchType)
+              }
+              value={currentSearchType}
+              className="rounded-r-only"
+            >
+              <option value="all">
+                {intl.formatMessage(globalMessages.all)}
+              </option>
+              <option value="movie">
+                {intl.formatMessage(globalMessages.movies)}
+              </option>
+              <option value="tv">
+                {intl.formatMessage(globalMessages.tvshows)}
+              </option>
+              <option value="person">
+                {intl.formatMessage(globalMessages.persons)}
+              </option>
+              <option value="collection">
+                {intl.formatMessage(globalMessages.collection)}
+              </option>
+            </select>
+          </div>
+        </div>
       </div>
       <ListView
         items={titles}
