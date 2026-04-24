a846ccfafd84474574cf8ef860f94b2f861bdb8a
gauthier-th <mail@gauthierth.fr>
2026-04-24 17:52:13 +0200

    chore(deps): regenerate TypeORM migrations

--- server/migration/postgres/1777045867383-UpgradeConnectTypeORM.ts
+++ server/migration/postgres/1777045867383-UpgradeConnectTypeORM.ts
@@ -1,7 +1,7 @@
 import type { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class UpgradeConnectTypeORM1776435152235 implements MigrationInterface {
-  name = 'UpgradeConnectTypeORM1776435152235';
+export class UpgradeConnectTypeORM1777045867383 implements MigrationInterface {
+  name = 'UpgradeConnectTypeORM1777045867383';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
