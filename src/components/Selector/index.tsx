8997f2fbc0367a214f8c975616e419e3166d3636
0xsysr3ll <31414959+0xSysR3ll@users.noreply.github.com>
2026-04-27 15:59:01 +0200

    fix: remove null type

--- src/components/Selector/index.tsx
+++ src/components/Selector/index.tsx
@@ -295,7 +295,7 @@ export const KeywordSelector = ({
 }: BaseSelectorMultiProps | BaseSelectorSingleProps) => {
   const intl = useIntl();
   const [selectedValue, setSelectedValue] = useState<
-    MultiValue<SingleVal> | SingleValue<SingleVal> | null
+    MultiValue<SingleVal> | SingleValue<SingleVal>
   >(null);
 
   useEffect(() => {
