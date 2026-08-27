fallenbagel/jellyseerr

feat(settings): add 'Hide Requested Media' option to settings
This PR introduces a new setting that allows users to hide media that has been requested but is not yet available from the "Discover" home page and related categories.

Signed-off-by: 0xsysr3ll <0xsysr3ll@pm.me>

src/components/Search/index.tsx
@@ -34,7 +34,7 @@ const Search = () => {
     {
       query: router.query.query,
     },
-    { hideAvailable: false, hideBlacklisted: false }
+    { hideAvailable: false, hideBlacklisted: false, hideRequested: false }
   );
 
   if (error) {

src/hooks/useDiscover.ts
@@ -15,6 +15,7 @@ interface BaseMedia {
   mediaType: string;
   mediaInfo?: {
     status: MediaStatus;
+    requests?: any[];
   };
 }
 
@@ -54,7 +55,7 @@ const useDiscover = <
 >(
   endpoint: string,
   options?: O,
-  { hideAvailable = true, hideBlacklisted = true } = {}
+  { hideAvailable = true, hideBlacklisted = true, hideRequested = true } = {}
 ): DiscoverResult<T, S> => {
   const settings = useSettings();
   const { hasPermission } = useUser();
@@ -131,6 +132,17 @@ const useDiscover = <
       (i) =>
         (i.mediaType === 'movie' || i.mediaType === 'tv') &&
         i.mediaInfo?.status !== MediaStatus.BLACKLISTED
+    );
+  }
+
+  if (settings.currentSettings.hideRequested && hideRequested) {
+    titles = titles.filter(
+      (i) =>
+        (i.mediaType === 'movie' || i.mediaType === 'tv') &&
+        (i.mediaInfo?.status === MediaStatus.AVAILABLE ||
+          i.mediaInfo?.status === MediaStatus.PARTIALLY_AVAILABLE ||
+          !i.mediaInfo?.requests ||
+          i.mediaInfo.requests.length === 0)
     );
   }
 

seerr-team/seerr

src/components/Search/index.tsx
@@ -34,7 +34,7 @@ const Search = () => {
     {
       query: router.query.query,
     },
-    { hideAvailable: false, hideBlocklisted: false }
+    { hideAvailable: false, hideBlocklisted: false, hideRequested: false }
   );
 
   if (error) {

src/hooks/useDiscover.ts
@@ -19,6 +19,7 @@ interface BaseMedia {
   mediaType: string;
   mediaInfo?: {
     status: MediaStatus;
+    requests?: any[];
   };
 }
 
@@ -58,7 +59,7 @@ const useDiscover = <
 >(
   endpoint: string,
   options?: O,
-  { hideAvailable = true, hideBlocklisted = true } = {}
+  { hideAvailable = true, hideBlocklisted = true, hideRequested = true } = {}
 ): DiscoverResult<T, S> => {
   const settings = useSettings();
   const { hasPermission } = useUser();
@@ -139,6 +140,17 @@ const useDiscover = <
       (i) =>
         (i.mediaType === 'movie' || i.mediaType === 'tv') &&
         i.mediaInfo?.status !== MediaStatus.BLOCKLISTED
+    );
+  }
+
+  if (settings.currentSettings.hideRequested && hideRequested) {
+    titles = titles.filter(
+      (i) =>
+        (i.mediaType === 'movie' || i.mediaType === 'tv') &&
+        (i.mediaInfo?.status === MediaStatus.AVAILABLE ||
+          i.mediaInfo?.status === MediaStatus.PARTIALLY_AVAILABLE ||
+          !i.mediaInfo?.requests ||
+          i.mediaInfo.requests.length === 0)
     );
   }
 